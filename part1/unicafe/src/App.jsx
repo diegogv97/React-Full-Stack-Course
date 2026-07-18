4830d2fdcad0b2e1b6cf80d70810f654813a778b
diegogv97 <diegogv97@gmail.com>
2026-07-18 10:15:04 -0600

    exercises 1.3 to 1.5

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,22 @@
+const Hello = ({name, age}) => {
+  const bornYear = () => new Date().getFullYear() - age
+
+  return (
+    <div>
+      <p>
+        Hello {name}, you are {age} years old
+      </p>
+       <p>So you were probably born in {bornYear()}</p>
+    </div>
+  )
+}
+
+const App = (props) => {
+  const {counter} = props
+  return (
+    <div>{counter}</div>
+  )
+}
+
+
+export default App
