cfa3cf9f525a4ef02262cf0d14882211f7030a53
diegogv97 <diegogv97@gmail.com>
2026-07-26 11:51:21 -0600

    unicafe done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,22 +1,70 @@
-const Hello = ({name, age}) => {
-  const bornYear = () => new Date().getFullYear() - age
+import { useState } from 'react'
 
+const Button = ({handleClick, text}) => {
   return (
-    <div>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-       <p>So you were probably born in {bornYear()}</p>
-    </div>
+    <>
+      <button onClick={handleClick}>{text}</button>
+    </>
   )
 }
 
-const App = (props) => {
-  const {counter} = props
+const StatisticLine = ({text, value}) => {
   return (
-    <div>{counter}</div>
+    <>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+    </>
   )
 }
 
+const Statistics = ({statistics}) => {
+  const {good, neutral, bad} = statistics
+  const total = good + neutral + bad;
+  
+  if(total === 0){
+    return (
+      <>
+        <p>No feedback given</p>
+      </>
+    )
+  }
+
+  const average = ((good * 1) + (neutral * 0) + (bad * -1)) / total
+  const positive = total > 0? (good / total) * 100 : 0;
+
+  return (
+  <>
+    <table>
+      <StatisticLine text={'good'} value={good}/>
+      <StatisticLine text={'neutral'} value={neutral}/>
+      <StatisticLine text={'bad'} value={bad}/>
+      <StatisticLine text={'total'} value={total}/>
+      <StatisticLine text={'average'} value={average}/>
+      <StatisticLine text={'positive'} value={positive+ '%'}/>
+    </table>
+  </>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <>
+      <h1>give feedback</h1>
+      <Button handleClick={() => setGood(good + 1)} text={'good'}/>
+      <Button handleClick={() => setNeutral(neutral + 1)} text={'neutral'}/>
+      <Button handleClick={() => setBad(bad + 1)} text={'bad'}/>
+
+      <h1>statistics</h1>
+      <Statistics statistics={{good, neutral, bad}}/>
+    </>
+  )
+}
 
 export default App
