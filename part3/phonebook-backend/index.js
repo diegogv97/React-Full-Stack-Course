9863c8cc70c10f9259fbd5594d24c6068ab33e57
diegogv97 <diegogv97@gmail.com>
2026-08-17 11:12:24 -0600

    excecise 3.22

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,140 +1,140 @@
-require("dotenv").config();
-const express = require("express");
-const morgan = require("morgan");
-const Person = require("./models/person");
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const Person = require('./models/person')
 
-const app = express();
+const app = express()
 
-app.use(express.static("dist"));
-app.use(express.json());
+app.use(express.static('dist'))
+app.use(express.json())
 
-morgan.token("body", (req, res) => JSON.stringify(req.body));
+morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(
   morgan((tokens, req, res) => {
     return [
       tokens.method(req, res),
       tokens.url(req, res),
       tokens.status(req, res),
-      tokens.res(req, res, "content-length"),
-      "-",
-      tokens["response-time"](req, res),
-      "ms",
+      tokens.res(req, res, 'content-length'),
+      '-',
+      tokens['response-time'](req, res),
+      'ms',
       tokens.body(req, res),
-    ].join(" ");
+    ].join(' ')
   }),
-);
+)
 
-app.get("/info", (request, response) => {
+app.get('/info', (request, response) => {
   Person.find({}).then((people) => {
-    const info = `<h2>Phonebook has info for ${people.length} people</h2>`;
-    const currentdate = `<p>${new Date()}</p>`;
-    response.send(info + currentdate);
-  });
-});
+    const info = `<h2>Phonebook has info for ${people.length} people</h2>`
+    const currentdate = `<p>${new Date()}</p>`
+    response.send(info + currentdate)
+  })
+})
 
-app.get("/api/persons", (request, response) => {
+app.get('/api/persons', (request, response) => {
   Person.find({}).then((people) => {
-    response.json(people);
-  });
-});
+    response.json(people)
+  })
+})
 
-app.get("/api/persons/:id", (request, response, next) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
     .then((person) => {
       if (person) {
-        return response.json(person);
+        return response.json(person)
       }
 
-      return response.status(404).end();
+      return response.status(404).end()
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.delete("/api/persons/:id", (request, response, next) => {
-  const id = request.params.id;
+app.delete('/api/persons/:id', (request, response, next) => {
+  const id = request.params.id
 
   Person.findByIdAndDelete(id)
     .then((deletedUser) => {
       if (!deletedUser) {
-        return response.status(404).end();
+        return response.status(404).end()
       }
 
-      return response.status(204).end();
+      return response.status(204).end()
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.put("/api/persons/:id", (request, response, next) => {
-  const id = request.params.id;
-  const { name, number } = request.body;
+app.put('/api/persons/:id', (request, response, next) => {
+  const id = request.params.id
+  const { name, number } = request.body
 
   if (!name || !number) {
     return response.status(400).json({
-      error: "content missing",
-    });
+      error: 'content missing',
+    })
   }
 
   Person.findById(id)
     .then((person) => {
       if (!person) {
-        return response.status(404).end();
+        return response.status(404).end()
       }
 
-      person.name = name;
-      person.number = number;
+      person.name = name
+      person.number = number
 
       return person.save({ runValidators: true }).then((updatedPerson) => {
-        response.json(updatedPerson);
-      });
+        response.json(updatedPerson)
+      })
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.post("/api/persons", (request, response, next) => {
-  const { name, number } = request.body;
+app.post('/api/persons', (request, response, next) => {
+  const { name, number } = request.body
 
   Person.findOne({ name })
     .then((foundPerson) => {
       if (foundPerson) {
-        return Promise.reject({ name: "DuplicatedName" });
+        return Promise.reject({ name: 'DuplicatedName' })
       }
 
-      const person = new Person({ name, number });
+      const person = new Person({ name, number })
       return person.save({ runValidators: true }).then((person) => {
-        return response.json(person);
-      });
+        return response.json(person)
+      })
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
-};
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
 // handler of requests with unknown endpoint
-app.use(unknownEndpoint);
+app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
+  console.error(error.message)
 
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "malformatted id" });
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
   }
 
-  if (error.name === "ValidationError") {
-    return response.status(400).json({ error: error.message });
+  if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
-  if (error.name === "DuplicatedName") {
-    return response.status(400).send({ error: "name must be unique" });
+  if (error.name === 'DuplicatedName') {
+    return response.status(400).send({ error: 'name must be unique' })
   }
 
-  next(error);
-};
+  next(error)
+}
 // this has to be the last loaded middleware, also all the routes should be registered before this!
-app.use(errorHandler);
+app.use(errorHandler)
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
--- part3/phonebook-backend/mongo.js
+++ part3/phonebook-backend/mongo.js
@@ -1,40 +1,40 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-  console.log("give password as argument");
-  process.exit(1);
+  console.log('give password as argument')
+  process.exit(1)
 }
 
-const password = process.argv[2];
-const url = `mongodb+srv://fullstack:${password}@fullstackcourse.kpednte.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=Cluster0`;
+const password = process.argv[2]
+const url = `mongodb+srv://fullstack:${password}@fullstackcourse.kpednte.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=Cluster0`
 
-mongoose.set("strictQuery", false);
-mongoose.connect(url, { family: 4 });
+mongoose.set('strictQuery', false)
+mongoose.connect(url, { family: 4 })
 
 const personSchema = new mongoose.Schema({
   name: String,
   number: String,
-});
+})
 
-const Person = mongoose.model("Person", personSchema);
+const Person = mongoose.model('Person', personSchema)
 
-const personProvided = process.argv.length === 5;
+const personProvided = process.argv.length === 5
 
 if (personProvided) {
-  const name = process.argv[3];
-  const number = process.argv[4];
-  const person = new Person({ name, number });
+  const name = process.argv[3]
+  const number = process.argv[4]
+  const person = new Person({ name, number })
 
-  person.save().then((result) => {
-    console.log(`added ${name} number ${number} to phonebook`);
-    mongoose.connection.close();
-  });
+  person.save().then(() => {
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
 } else {
-  console.log("phonebook");
+  console.log('phonebook')
   Person.find({}).then((result) => {
     result.forEach((person) => {
-      console.log(`${person.name} ${person.number}`);
-    });
-    mongoose.connection.close();
-  });
+      console.log(`${person.name} ${person.number}`)
+    })
+    mongoose.connection.close()
+  })
 }
